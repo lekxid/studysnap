313faf8dca61a15d95aafc17f8726b06c6ed1850
Adeleke Gregory Ajayi <lekxid@gmail.com>
2026-04-01 22:44:06 +0000

    fix secret key + config

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,20 +1,20 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class Settings(BaseSettings):
     app_name: str = "StudySnap AI"
     app_env: str = "development"
     app_host: str = "0.0.0.0"
     app_port: int = 8000
 
-    DATABASE_URL = "sqlite:///./test.db"
-    openai_api_key: str = ""
+    DATABASE_URL: str = "sqlite:///./test.db"
 
-    secret_key: str
+    secret_key: str = "supersecretkey"
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 60
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
 
 settings = Settings()
