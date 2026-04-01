c3868c6ac57a5c41700f4ea3ba23d60146d900ab
Adeleke Gregory Ajayi <lekxid@gmail.com>
2026-04-01 23:18:11 +0000

    fix database import final

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
-from app.database import Base
+from backend.app.database import Base
 
 
 class User(Base):
